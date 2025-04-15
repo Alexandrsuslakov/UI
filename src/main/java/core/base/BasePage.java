3f2cfde57bc7cb3585dc151d7fab8232d01b82e0
asusliakov <alexandrsuslakov@gmail.com>
2025-04-15 16:34:52 +0300

    Создание базового теста.

--- src/main/java/core/base/BasePage.java
+++ src/main/java/core/base/BasePage.java
@@ -1,7 +1,8 @@
-package core.base.pages;
+package core.base;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -12,15 +13,20 @@ public class BasePage {
     //Общие элементы, которые могут использоваться на разных страницах
     protected SelenideElement headerLogo = $("div.logo");
     protected SelenideElement searchField = $x("[@name= 'st.query']");
-    protected SelenideElement notoficationIcon = $("div.notifications");
+    protected SelenideElement vkServices = $("div.notifications");
 
     // Метод для поиска по сайту
     public void search(String query){
-        searchField.shouldBe('visible').setValue(query).pressEnter();
+        searchField.shouldBe(visible).setValue(query).pressEnter();
     }
 
-    public void openNotofication {
-        notoficationIcon.shouldBe('visible').click();
-        System.out.println("");
+    public void openvkServices() {
+        vkServices.shouldBe(visible).click();
     }
+
+    //Клик на логотип ОК
+    public void clickLogo(){
+        headerLogo.shouldBe(visible).click();
+    }
+
 }
--- src/main/java/core/base/BaseTest.java
+++ src/main/java/core/base/BaseTest.java
@@ -1,9 +1,9 @@
 package core.base;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import Configuration;
-import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+
 import java.io.InputStream;
 import java.util.Properties;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -13,32 +13,32 @@ public class BaseTest {
     // Базовый URL
     protected static String baseUrl;
 
-    @BeforeAll
-    public static void setUp(){
-        baseUrl = determinationBaseUrl ();
+
+    @BeforeEach
+    public void setUp(){
         Configuration.browser ="chrome";
         Configuration.browserSize = "1920x1080";
+        baseUrl = determinationBaseUrl();
     }
 
     private static String determinationBaseUrl(){
-        String enviroment =System.getProperty("env","test");
-        String configFileName = "application" + enviroment + ".properties";
+        String enviroment = System.getProperty("env","test");
+        String configFileName = "application-" + enviroment + ".properties";
 
         Properties properties = new Properties();
-        try (InputStream input = BaseTest.class.getClassLoader().getResourceAsStream("configFileName") {
+        try (InputStream input = BaseTest.class.getClassLoader().getResourceAsStream(configFileName)) {
             if (input == null) {
-                throw new IllegalAccessException("Configuration file not found: " + configFileName);
+                throw new IllegalStateException("Configuration file not found: " + configFileName);
             }
             properties.load(input);
-        } catch (IOException e) {
-            throw new IllegalAccessException("Unable to load configuration file: " + configFileName, e)
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to load configuration file: " + configFileName, e);
         }
         return properties.getProperty("baseUrl");
             }
             public String getBaseUrl() {
         return baseUrl;
         }
-
         @AfterEach
     public void tearDown() {
         closeWebDriver();
